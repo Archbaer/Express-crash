5412639a63fe19f867a45ea431adaee9f5b101a5
archbaer <pereirakebaili@gmail.com>
2024-12-03 21:29:20 +0000

    custom error handling

--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,10 @@ let posts = [
     {id: 3, title: 'Post three'}
 ];
 
+
+
 // Get all posts
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     const limit = parseInt(req.query.limit);
     
     if (!isNaN(limit) && limit > 0) {
@@ -19,26 +21,30 @@ router.get('/', (req, res) => {
 });
 
 // Get single post
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
     
     if (!post) {
-        return res.status(404).json({ msg: `A post with the id of ${id} was not found` });
+        const error = new Error(`A post with the id of ${id} was not found`);
+        error.status = 404;
+        return next(error);
     }
     
     res.status(200).json(post);
 });
 
 // Create new post
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     const newPost = {
         id: posts.length + 1,
         title: req.body.title
     };
 
     if (!newPost.title) {
-        return res.status(400).json({ message: 'Please include a title' })
+        const error = new Error(`Please include a title`);
+        error.status = 400;
+        return next(error);
     }
 
     posts.push(newPost);
@@ -46,7 +52,7 @@ router.post('/', (req, res) => {
 });
 
 // Update Post
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
@@ -59,7 +65,7 @@ router.put('/:id', (req, res) => {
 });
 
 // Delete post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
@@ -72,3 +78,5 @@ router.delete('/:id', (req, res) => {
 });
 
 export default router;
+
+// Estive aqui
